5aecc3699cc36b22fbe46b0433d69bd8976e40ce
chandrakalaM08 <chandrakala.2508@gmail.com>
2023-03-31 02:13:59 +0530

    day-3 task

--- src/index.js
+++ src/index.js
@@ -6,8 +6,7 @@ import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <div>Thi sis div</div>
-  // <BrowserRouter>
-  //   <div>This is in div</div>
-  // </BrowserRouter>
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
 );
